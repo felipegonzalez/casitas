6694c264ba68c4c0436818dadfeeb9260a740966
Felipe González <felipexgonzalez@gmail.com>
2017-06-21 16:15:22 -0500

    Add first version of web app for controlling home

---
+++ webapp/casa_app.py
@@ -0,0 +1,190 @@
+import os, os.path
+import random
+import string
+import json
+#import sqlite3 as lite
+import HTML
+import cherrypy
+import os, sys
+sys.path.insert(0,os.path.pardir)
+from settings import r 
+
+cherrypy.server.socket_host = '0.0.0.0'
+
+
+class control(object):
+
+    @cherrypy.expose
+    def controlcasa(self):
+        return open('index_main.html')
+
+
+    #@cherrypy.expose
+    #def autoluz(self):
+        #con = lite.connect('/Volumes/mmshared/bdatos/comandos.db')
+        #with con:
+        #    cur = con.cursor()
+        #    commandx = "INSERT INTO pendientes VALUES('auto_luces','0')"
+        #    cur.execute(commandx)
+        #return 'Auto luces' 
+
+
+
+    #@cherrypy.expose
+    #def alarmas_reset(self):
+    #    con = lite.connect('/Volumes/mmshared/bdatos/comandos.db')
+    #    with con:
+    #        cur = con.cursor()
+    #        commandx = "INSERT INTO pendientes VALUES('alarmas_reset','0')"
+    #        cur.execute(commandx)
+    #    return 'Reestableciendo alarmas'
+
+
+    # @cherrypy.expose
+    # def garage(self):
+    #     con = lite.connect('/Volumes/mmshared/bdatos/comandos.db')
+    #     with con:
+    #         cur = con.cursor()
+    #         commandx = "INSERT INTO pendientes VALUES('abrir_garage','1')"
+    #         cur.execute(commandx)
+    #     return 'Activando garage'
+
+    # @cherrypy.expose
+    # def alarma(self, sw):
+    #     con = lite.connect('/Volumes/mmshared/bdatos/comandos.db')
+    #     with con:
+    #         cur = con.cursor()
+    #         if(sw=='1'):
+    #             commandx = "INSERT INTO pendientes VALUES('activar_alarma','1')"
+    #             mensaje = 'Activando alarma'
+    #         else:
+    #             commandx = "INSERT INTO pendientes VALUES('activar_alarma','0')"
+    #             mensaje = 'Desactivando alarma'
+    #         cur.execute(commandx)
+    #     return mensaje
+
+    # @cherrypy.expose
+    # def chapa(self, sw):
+    #     con = lite.connect('/Volumes/mmshared/bdatos/comandos.db')
+    #     with con:
+    #         cur = con.cursor()
+    #         if(sw=='1'):
+    #             commandx = "INSERT INTO pendientes VALUES('chapa','1')"
+    #             mensaje ='Cerrar chapa'
+    #         if(sw=='0'):
+    #             commandx = "INSERT INTO pendientes VALUES('chapa','0')"
+    #             mensaje = 'Abrir chapa'
+    #         cur.execute(commandx)
+    #     return mensaje
+
+    @cherrypy.expose
+    def regar(self, sw):
+        mensaje = 'Prueba'
+        if(sw=='1'):
+            r.publish('commands', json.dumps({"device_name":"caja_goteo",
+                "command":"turn_on", "value":"regar"
+                }))
+        if(sw=='0'):
+            r.publish('commands', json.dumps({'device_name':'caja_goteo',
+                'command':'turn_off', 'value':'regar'
+                }))  
+        # con = lite.connect('/Volumes/mmshared/bdatos/comandos.db')
+        # with con:
+        #     cur = con.cursor()
+        #     if(sw=='1'):
+        #         commandx = "INSERT INTO pendientes VALUES('regar','1')"
+        #         mensaje ='Regar patio'
+        #     if(sw=='0'):
+        #         commandx = "INSERT INTO pendientes VALUES('regar','0')"
+        #         mensaje = 'Dejar de regar patio'
+        #     cur.execute(commandx)
+        return mensaje
+
+class infoBasica(object):
+    exposed = True
+    @cherrypy.tools.accept(media='text/plain')
+    def GET(self, resp=''):
+        # con2 = lite.connect('/Volumes/mmshared/bdatos/ultimas.db')
+        # with con2:
+        #     cur = con2.cursor()
+        #     commandx = "SELECT timestamp, medicion, valor from Status WHERE lugar = 'global' ORDER BY medicion" # OR (lugar='tv' AND medicion='temperature') "
+        #     #commandx = "SELECT timestamp,medicion,valor,lugar from Status  ORDER BY lugar, medicion "
+        #     res = cur.execute(commandx)
+        # tabla = HTML.table(res).split('\n')
+        # tabla2 = " ".join(tabla[1:(len(tabla)-1)])
+        # tabla3 =  "<table class='table table-striped lead'>"+tabla2+"</table>"
+        # con2.close()
+        # return tabla3
+        return "Test"
+
+
+
+
+
+
+
+
+
+
+
+# class lucesCocina(object):
+#      exposed = True
+#      @cherrypy.tools.accept(media='text/plain')
+#      def POST(self, resp=''):
+#         con = lite.connect('/Volumes/mmshared/bdatos/comandos.db')
+#         with con:
+#             cur = con.cursor()
+#             commandx = "INSERT INTO pendientes VALUES('luces_cocina','1')"
+#             cur.execute(commandx)
+#         return 'Apagando luces' 
+
+
+# class apagarCocina(object):
+#      exposed = True
+#      @cherrypy.tools.accept(media='text/plain')
+#      def POST(self, resp=''):
+#         con = lite.connect('/Volumes/mmshared/bdatos/comandos.db')
+#         with con:
+#             cur = con.cursor()
+#             commandx = "INSERT INTO pendientes VALUES('luces_cocina','0')"
+#             cur.execute(commandx)
+#         return 'Apagando luces' 
+
+
+
+
+if __name__ == '__main__':
+    cherrypy.config.update({'server.socket_port':8090})
+    conf = {
+         '/': {
+             'tools.sessions.on': True,
+             'tools.staticdir.root': os.path.abspath(os.getcwd())
+         },
+         '/info_bas': {
+             'request.dispatch': cherrypy.dispatch.MethodDispatcher(),
+             'tools.response_headers.on': True,
+             'tools.response_headers.headers': [('Content-Type', 'text/plain')],
+         },
+
+         '/dist/css/bootstrap.min.css': {
+		'tools.staticfile.on' : True,
+		'tools.staticfile.filename' : "/Users/felipe/casitas/webapp/dist/css/bootstrap.min.css"
+        },
+        '/dist/jquery/jquery-2.0.3.min.js': {
+        'tools.staticfile.on' : True,
+        'tools.staticfile.filename' : "/Users/felipe/casitas/webapp/dist/jquery/jquery-2.0.3.min.js"
+        },
+        '/dist/app.js':{
+            'tools.staticfile.on' : True,
+            'tools.staticfile.filename' : "/Users/felipe/casitas/webapp/dist/app.js"
+        },
+        '/img':{
+            'tools.staticdir.on' : True,
+            'tools.staticdir.dir' : "/Users/felipe/casitas/webapp/img"
+        }
+}
+    webapp = control()
+    webapp.info_bas = infoBasica()
+
+    cherrypy.quickstart(webapp, '/', conf)
+
---
+++ webapp/dist/app.js
@@ -0,0 +1,157 @@
+   $(document).ready(function() {
+         
+         $.get("/info_bas", {})
+         .done(function(string){
+
+            $("#estado").append(string);
+
+         });
+         $("#autoluces").click(function(e) {
+           $.post("/autoluz", {}).done(function(string) {
+               $("#respuesta input").val(string);
+            });
+           e.preventDefault();
+         });
+         
+         $("#alarmas-reset").click(function(e) {
+           $.post("/alarmas_reset", {}).done(function(string) {
+               $("#respuesta input").val(string);
+            });
+           e.preventDefault();
+         });
+
+         $("#autoaire").click(function(e) {
+           $.post("/auto_ac", {}).done(function(string) {
+               $("#respuesta input").val(string);
+            });
+           e.preventDefault();
+           setTimeout(function(){location.reload()}, 2000);
+         });
+          
+          $("#accion-garage").click(function(e) {
+           $.post("/garage", {}).done(function(string) {
+               $("#respuesta input").val(string);
+            });
+           e.preventDefault();
+           setTimeout(function(){location.reload()}, 2000);
+
+         });
+         $("#accion-zumbador").click(function(e) {
+           $.post("/zumbador", {})
+            .done(function(string) {
+               $("#respuesta input").val(string);
+            });
+           e.preventDefault();
+         });
+          $("#accion-alarma").click(function(e) {
+           $.post("/alarma/1", {}).done(function(string) {
+               $("#respuesta input").val(string);
+            });
+           e.preventDefault();
+           setTimeout(function(){location.reload()}, 2000);
+
+         });
+     $("#accion-alarma2").click(function(e) {
+           $.post("/alarma/0", {}).done(function(string) {
+               $("#respuesta input").val(string);
+
+            });
+           e.preventDefault();
+           setTimeout(function(){location.reload()}, 2000);
+
+         });
+      $("#aire").click(function(e) {
+           $.post("/control_aire", {}).done(function(string) {
+               $("#respuesta input").val(string);
+            });
+           e.preventDefault();
+         });
+        $("#dormir-a").click(function(e) {
+           $.post("/dormir", {})
+            .done(function(string) {
+               $("#respuesta input").val(string);
+
+            });
+           e.preventDefault();
+         });
+        $("#lock-a").click(function(e) {   
+         $.post("/chapa/1", {}).done(function(string) {
+               $("#respuesta input").val(string);
+            }); 
+            e.preventDefault();
+           setTimeout(function(){location.reload()}, 2000);
+         });
+        $("#unlock-a").click(function(e) {
+           $.post("/chapa/0", {}).done(function(string) {
+               $("#respuesta input").val(string);
+            });
+           e.preventDefault();
+           setTimeout(function(){location.reload()}, 2000);
+         });
+        $("#regar-on").click(function(e) {
+           $.post("/regar/1", {}).done(function(string) {
+               $("#respuesta input").val(string);
+            });
+           e.preventDefault();
+           setTimeout(function(){location.reload()}, 2000);
+         });
+        $("#regar-off").click(function(e) {   
+         $.post("/regar/0", {}).done(function(string) {
+               $("#respuesta input").val(string);
+            }); 
+            e.preventDefault();
+           setTimeout(function(){location.reload()}, 2000);
+         });
+
+
+     
+        $("#luces-cocina").click(function(e) {
+           $.post("/lucescocina", {})
+            .done(function(string) {
+               $("#respuesta input").val(string);
+
+            });
+           e.preventDefault();
+         });
+
+        $("#llegar-caminando").click(function(e){
+          $.post("/zumbador", {})
+            .done(function(string) {
+            });
+          $.post("/chapa/0", {})
+            .done(function(string) {
+               $("#respuesta input").val(string);
+
+            });
+           e.preventDefault();
+        });
+
+        $("#llegar-coche").click(function(e){
+          $.post("/garage", {})
+            .done(function(string) {
+            });
+          $.post("/chapa/0", {})
+            .done(function(string) {
+               $("#respuesta input").val(string);
+
+            });
+           e.preventDefault();
+        });
+
+        $("#apagar-cocina").click(function(e) {
+           $.post("/apagarcocina", {})
+            .done(function(string) {
+               $("#respuesta input").val(string);
+            });
+           e.preventDefault();
+         });
+      $("#apagar-luces").click(function(e) {
+           $.post("/apagar_luces", {}).done(function(string) {
+               $("#respuesta input").val(string);
+            });
+           e.preventDefault();
+           setTimeout(function(){location.reload()}, 2000);
+
+         });
+  
+       });
