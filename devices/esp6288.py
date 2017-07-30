837d28d96668a9d197cbc6dba1cea49e73b21578
Felipe González <felipexgonzalez@gmail.com>
2017-07-30 14:23:05 -0500

    Define treatment of http connected device (for esp6288)

--- devices/esp6288.py
+++ devices/esp6288.py
@@ -14,8 +14,33 @@ class Esp6288(object):
 
 
     def parse(self, message):
+        # general response 
         parsed_m = []
+        message_p = json.loads(message['data'])
+        if(isinstance(message_p, dict)):
+            if(message_p['type'] == 'status'):
+                for elem in message_p['events']:
+                    #self.status[elem] = message_p['values'][elem]
+                    parsed_m.append(json.loads(message_p['events'][elem]))
         return parsed_m
 
     def update(self, state):
+        return
+
+    #generic way of running a command on an esp6288 box
+    def _send_get(self, command):
+        subdevice = command['value']
+        address = self.ip_address + '/' + subdevice + '/' + command['command']
+        new_message = json.dumps({'device_name':self.name, 'address':address, 
+            'payload':'', 'type':'get'})
+        self.messager.publish('http-commands', new_message)
+        return 
+
+    # we define these functions which are available to the system:
+    def turn_on(self, command, state):
+        self._send_get(command)
+        return
+
+    def turn_off(self, command, state):
+        self._send_get(command)
         return
