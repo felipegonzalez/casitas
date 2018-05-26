a1adfdaf7fe3fac9d5940b08399a3c8cbef09adf
Felipe González <felipexgonzalez@gmail.com>
2018-05-26 16:22:32 -0500

    Remove hack for garage light

--- apps/appdoorlight.py
+++ apps/appdoorlight.py
@@ -52,11 +52,14 @@ class AppDoorLight():
     def check_command(self, comm_content,  state):
         fire = False
         value = ''
-        if comm_content:
-            if(comm_content['value']=='garage_open'):
-                places_fire = self.door_mapping['hall_entrada']
-                for ll in places_fire:
-                    pl = self.place_lights[ll]
-                    if(int(state['photo'][pl]) < int(state['min_photo'][pl])):
-                        fire = fire or True
+        #if comm_content:
+        #    if(comm_content['value']=='garage_open'):
+        #        state['last_motion']['patio'] = state['timestamp']
+        #        state['last_motion']['escaleras_patio'] = state['timestamp']
+        #        state['last_motion']['hall_entrada'] = state['timestamp']
+                #places_fire = self.door_mapping['hall_entrada']
+                #for ll in places_fire:
+                #    pl = self.place_lights[ll]
+                #    if(int(state['photo'][pl]) < int(state['min_photo'][pl])):
+                #        fire = fire or True
         return fire, value
